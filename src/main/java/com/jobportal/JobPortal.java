842f59d50779a6f7914678d6b049ce633bb869d5
aravind-dasarat <aravind.dasarat@gmail.com>
2023-05-17 08:52:32 -0400

    code to init springboot

---
+++ src/main/java/com/jobportal/JobPortal.java
@@ -0,0 +1,16 @@
+package com.jobportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author aravind
+ */
+
+@SpringBootApplication
+public class JobPortal {
+    public static void main(String[] args) {
+        SpringApplication.run(JobPortal.class, args);
+    }
+}
